9300901915306d0f0c97897a7058df11424a4e67
Rayna M Harris <raynamharris@users.noreply.github.com>
2025-07-13 23:35:02 -0700

    better now

--- app.R
+++ app.R
@@ -19,6 +19,13 @@ library(bslib)
 #library(rsconnect)
 #rsconnect::writeManifest()
 
+
+colsofinterest <- c("Type", "Density", "Electronegativity", "Metal" , "NumIsotopes",
+                    "Phase" ,  "Radioactive", "Radius", "ValenceNum", "Mass" )
+
+quals <- c("Type","Metal" , "Phase",  "Radioactive", "ValenceNum" )
+quants <- c("Density", "Electronegativity", "Mass", "NumIsotopes","Radius" )
+
 df <- read_excel("Lab.XX_DataAnalysisofAtoms.xlsx") %>%
   filter(AtomicRadius > 0)  %>%
   mutate(NumberofValence = as.factor(NumberofValence),
@@ -28,12 +35,11 @@ df <- read_excel("Lab.XX_DataAnalysisofAtoms.xlsx") %>%
          "Mass" = "AtomicMass",
          "AtomicNum" = "AtomicNumber",
          "ValenceNum" = "NumberofValence",
-         "Radius"= "AtomicRadius")
+         "Radius"= "AtomicRadius") %>%
+  select(AtomicNum, Group, Period, Symbol, Element, NumberofProtons, all_of(colsofinterest)) %>%
+  mutate(NewLabel = paste(AtomicNum, Symbol, sep = "\n"))
 head(df)
 
-colsofinterest <- c("Type", "Density", "Electronegativity", "Metal" , "NumIsotopes",
-                      "Phase" ,  "Radioactive", "ValenceNum" )
-colsofinterest
 
 types <- levels(df$Type)
 types
@@ -43,29 +49,30 @@ types
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Plot Physical Properties of Elements on the Periodic Table"),
+    titlePanel("Physical Properties of Elements on the Periodic Table"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            radioButtons("colsofinterest", label = "Plot Physical Properties", choices = colsofinterest),
+            #radioButtons("colsofinterest", label = "Visualize Qualitative Properties", choices = colsofinterest),
+            h4("Customize the Periodic Table"),
+            radioButtons("quals", label = "Color: Qualitative Properties", choices = quals),
+            radioButtons("quants", label = "Scale: Quantitative Prorties", choices = quants),
             br(),
-            br(),
-            br(),
-            radioButtons("types", label = "Explore the Dataset by Type", choices = types),
+            h4("Explore the Dataset"),
+            radioButtons("types", label = "Filter the Elements by Type", choices = types),
         ),
     
 
          #Show a plot of the generated distribution
         mainPanel(
-           plotOutput("radiobutton"),
+          br(),
+          plotOutput("radiobutton"),
            #plotOutput("radiobutton2"),
            #plotOutput("radiobutton3"),
+           br(),
            tableOutput('table')
         )
-        
-      
-        
     )
 )
 
@@ -73,23 +80,21 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$radiobutton <- renderPlot({
-
+      
       df2 <- df %>%
-        select(AtomicNum, Mass, Group, Period, Symbol, NumberofProtons, Radius, input$colsofinterest) %>%
-        rename("Variable" = input$colsofinterest) %>%
-        mutate(NewLabel = paste(AtomicNum, Symbol, sep = "\n"))
-      head(df2$NewLabel)
-
+        rename("Variable" = input$quals,
+               "Measure" = input$quants)
+    
       p <- ggplot(df2, aes(x = Group, y = Period, 
                            label = NewLabel, 
                            color = Variable)) +
-        geom_point(aes(size = Radius)) +
+        geom_point(aes(size = Measure)) +
         geom_text(check_overlap = TRUE, nudge_y = 0.5,
-                  size = 3) +
+                  size = 4) +
         scale_y_reverse(breaks= c(1,2,3,4,5,6,7)) +
-        scale_x_continuous(breaks = c(2,4,6,8,10,12,14,16,18)) +
-        theme_bw() +
-        labs(color = input$colsofinterest, 
+        scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,11,10,12,13,14,15,16,17,18)) +
+        theme_classic() +
+        labs(color = input$quals, size = input$quants,
              title = "Periodic Table of Elements",
              subtitle = input$colsofinterest) +
         theme(legend.position = "bottom")
@@ -102,18 +107,24 @@ server <- function(input, output) {
     output$radiobutton2 <- renderPlot({
       
       df2 <- df %>%
-        select(Group, Period, Symbol,AtomicNum, NumberofNeutrons, Mass, Radius, input$colsofinterest) %>%
-        rename("Variable" = input$colsofinterest)
+        #filter(Type == input$types) %>%
+        rename("Variable" = input$quals,
+               "Measure" = input$quants) 
+        
       
       p <- ggplot(df2, aes(x = AtomicNum, y = Variable, 
-                           label = Symbol, color = Variable)) +
-        geom_point(aes(size = Radius)) +
-        theme_bw() +
+                           label = NewLabel, color = Variable)) +
+        geom_point(aes(size = Measure)) +
+        geom_text(check_overlap = TRUE, nudge_y = 0.2,
+                  size = 3) +
+        theme_classic() +
         labs(y = input$colsofinterest,
              color = input$colsofinterest, 
              title = "Elements Organized by Atomic Number",
              subtitle = input$colsofinterest) +
-        theme(legend.position = "bottom")
+        theme(legend.position = "bottom", 
+              ) +
+        scale_x_continuous(breaks = c(1,3,11,19,37,55,87), limits = c(0,87)) 
       print(p)
     })
     
@@ -124,7 +135,7 @@ server <- function(input, output) {
         mutate(AtomicNum = as.factor(AtomicNum),
                Group = as.factor(Group),
                Period = as.factor(Period)) %>%
-        select(AtomicNum, Symbol, Element, Mass, Group, Period, Radius, input$colsofinterest) 
+        select(AtomicNum, Symbol, Element, Group, Period, input$quals, input$quants) 
       df2
     })
     
