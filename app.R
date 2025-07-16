253e31037da44408b051346feb9edc5a2407f210
Rayna M Harris <raynamharris@users.noreply.github.com>
2025-07-16 15:45:57 -0700

    set size of periodic table

--- app.R
+++ app.R
@@ -80,7 +80,7 @@ ui <- fluidPage(
                                choices = phases, selected = phases),
             br(),
             checkboxGroupInput("types", label = "Select: Element Type", 
-                               choices = types, selected = types),
+                               choices = types, selected = displayfirst),
             br(),
             downloadButton("downloadTable", "Download the table displayed"),
             br(),
@@ -144,8 +144,10 @@ server <- function(input, output) {
         geom_point(aes(size = Measure), shape = 15) +
         geom_text(check_overlap = TRUE, nudge_y = 0.5,
                   size = 4) +
-        scale_y_reverse(breaks= c(1,2,3,4,5,6,7)) +
-        scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,11,10,12,13,14,15,16,17,18)) +
+        scale_y_reverse(breaks= c(1,2,3,4,5,6,7),
+                        limits = c(10,1)) +
+        scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,11,10,12,13,14,15,16,17,18),
+                           limits = c(1,18)) +
         theme_classic() +
         labs(color = input$quals, 
              size = input$quants,
